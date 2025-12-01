ad029c3def4bb3ec0b031da54a4e54ab1b42d8f2
KhuzemahHQ <khuzemahh@gmail.com>
2025-12-01 11:03:50 -0500

    Creating python files for data ingestion and baseline training

---
+++ baseline.py
@@ -0,0 +1,3 @@
+train_dataset = load_dataset("LabHC/bias_in_bios", split='train')
+test_dataset = load_dataset("LabHC/bias_in_bios", split='test')
+dev_dataset = load_dataset("LabHC/bias_in_bios", split='dev')
